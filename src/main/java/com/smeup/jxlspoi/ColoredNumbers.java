f4b23cdf76283178bb2afa51a4e52b4ba4b4410d
MauroWB <maurobellinzona88@gmail.com>
2019-06-27 12:08:35 +0200

    Ottimizzato ColoredNumbers

--- src/main/java/com/smeup/jxlspoi/ColoredNumbers.java
+++ src/main/java/com/smeup/jxlspoi/ColoredNumbers.java
@@ -5,7 +5,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.apache.poi.ss.usermodel.Cell;
@@ -26,34 +25,23 @@ public class ColoredNumbers {
 		InputStream in = new FileInputStream("src/main/resources/excel/cpcpoi_output.xlsx");
 		OutputStream out = new FileOutputStream("src/main/resources/excel/cpcpoinumber_output.xlsx");
 		Workbook workbook = new XSSFWorkbook(in);
-		//Se dichiaro lo stile qua fuori me lo applica ai campi giusti
-		/*CellStyle style = workbook.createCellStyle();
-		Font font = workbook.createFont();
-		font.setBold(true);
-		style.setFont(font);*/
-		Sheet sheet = workbook.getSheetAt(0);
-		Iterator<Row> iRow = sheet.rowIterator();
-		ArrayList<Cell> listC = new ArrayList<>();
-		while (iRow.hasNext()) {
-			Row nextRow = iRow.next();
-			Iterator<Cell> iCell = nextRow.cellIterator();
-			while (iCell.hasNext()) {
-				Cell nextCell = iCell.next();
-				if (nextCell.getCellType()==CellType.NUMERIC)
-					listC.add(nextCell);
-			}
-		}
-		
 		CellStyle numStyle = workbook.createCellStyle();
 		Font numFont = workbook.createFont();
 		numFont.setBold(true);
 		numStyle.setFont(numFont);
 		numStyle.setFillForegroundColor(IndexedColors.GREY_50_PERCENT.getIndex());
 		numStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-		
-		for (Cell i : listC)
-			i.setCellStyle(numStyle);
-		
+		Sheet sheet = workbook.getSheetAt(0);
+		Iterator<Row> iRow = sheet.rowIterator();
+		while (iRow.hasNext()) {
+			Row nextRow = iRow.next();
+			Iterator<Cell> iCell = nextRow.cellIterator();
+			while (iCell.hasNext()) {
+				Cell nextCell = iCell.next();
+				if (nextCell.getCellType() == CellType.NUMERIC)
+					nextCell.setCellStyle(numStyle);
+			}
+		}
 		workbook.write(out);
 		workbook.close();
 		in.close();
