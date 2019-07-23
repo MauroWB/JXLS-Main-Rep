8ee00c731c900bc1fabee8d2d8fafcab3f4d9282
MauroWB <maurobellinzona88@gmail.com>
2019-07-23 17:49:28 +0200

    Miglioramenti

--- src/main/test/com/smeup/commands/StyleCommand.java
+++ src/main/test/com/smeup/commands/StyleCommand.java
@@ -51,6 +51,8 @@ public class StyleCommand extends AbstractCommand {
 		
 		styleName = styleName.trim().toUpperCase();
 		
+		CellStyle cs = wb.createCellStyle();
+		
 		int lastRow = cellRef.getRow() + resultSize.getHeight() - 1;
 		int lastCol = cellRef.getCol() + resultSize.getWidth() - 1;
 		// "-1" Perché i valori di sopra non sono 0 based
@@ -61,9 +63,8 @@ public class StyleCommand extends AbstractCommand {
 					main.createRow(row);
 				Cell c = main.getRow(row).getCell(col, MissingCellPolicy.CREATE_NULL_AS_BLANK);
 				
-				for (Row iR : settings) 
+				for (Row iR : settings) { 
 					for (Cell iC : iR) {
-						{
 							if (iC.getCellType().equals(CellType.STRING)
 									&& styleName.equals(iC.getStringCellValue().trim().toUpperCase())) {
 
@@ -76,40 +77,41 @@ public class StyleCommand extends AbstractCommand {
 								if (iR.getCell(iC.getColumnIndex() + 5).getCellType().equals(CellType.STRING))
 									fillType = iR.getCell(iC.getColumnIndex() + 5).getStringCellValue();
 
-								if (iR.getCell(iC.getColumnIndex() + 4).getCellType().equals(CellType.BOOLEAN)) { 
+								if (iR.getCell(iC.getColumnIndex() + 4).getCellType().equals(CellType.BOOLEAN)) 
 									bold = iR.getCell(iC.getColumnIndex() + 4).getBooleanCellValue();
-									System.out.println(bold);
+								System.out.println(bold);
+
+								Font f = wb.createFont();
+								f.setBold(bold);
+								cs.setFont(f);
+
+								// Gestione errori per evitare che l'input sbagliato dell'utente interrompa
+								// l'esecuzione del programma.
+								try {
+									cs.setFillForegroundColor(IndexedColors.valueOf(color).index);
+								} catch (Exception e) {
+									System.out.println("Foreground Color non riconosciuto. Resetto a WHITE...");
+									cs.setFillForegroundColor(IndexedColors.WHITE.index);
 								}
+
+								try {
+									cs.setFillPattern(FillPatternType.valueOf(fillType));
+								} catch (Exception e) {
+									System.out.println("FillPatternType non riconosiuto. Resetto a SOLID_FOREGROUND...");
+									cs.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+								}
+								
 								stop = true;
 								break;
 							}
 						}
-						if (stop)
-							break;
-					}
+					if (stop)
+						break;
+				}
+						
 
 				// Nessun controllo visto che nessun attributo può essere null a questo punto
-				CellStyle cs = wb.createCellStyle();
-
-				Font f = wb.createFont();
-				f.setBold(bold);
-				cs.setFont(f);
-
-				// Gestione errori per evitare che l'input sbagliato dell'utente interrompa
-				// l'esecuzione del programma.
-				try {
-					cs.setFillForegroundColor(IndexedColors.valueOf(color).index);
-				} catch (Exception e) {
-					System.out.println("Foreground Color non riconosciuto. Resetto a WHITE...");
-					cs.setFillForegroundColor(IndexedColors.WHITE.index);
-				}
-
-				try {
-					cs.setFillPattern(FillPatternType.valueOf(fillType));
-				} catch (Exception e) {
-					System.out.println("FillPatternType non riconosiuto. Resetto a SOLID_FOREGROUND...");
-					cs.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-				}
+				
 				c.setCellStyle(cs);
 			}
 		}
