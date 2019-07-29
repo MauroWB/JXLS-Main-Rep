4fdba29a44b463d6f90f5549bbbe2060e6e604bf
MauroWB <maurobellinzona88@gmail.com>
2019-07-29 12:20:37 +0200

    Aggiunta lettura percorsi foglio FUN, processing image path.

--- src/main/test/com/smeup/test/UseCustomCommand.java
+++ src/main/test/com/smeup/test/UseCustomCommand.java
@@ -1,5 +1,6 @@
 package com.smeup.test;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -7,10 +8,11 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.List;
 
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.jxls.area.Area;
 import org.jxls.builder.AreaBuilder;
 import org.jxls.builder.xls.XlsCommentAreaBuilder;
-import org.jxls.common.CellRef;
 import org.jxls.common.Context;
 import org.jxls.transform.Transformer;
 import org.jxls.util.TransformerFactory;
@@ -19,13 +21,19 @@ import com.smeup.commands.ColorDemoCommand;
 import com.smeup.commands.StyleCommand;
 import com.smeup.commands.TemplateCommand;
 import com.smeup.commands.TestCustomCommand;
+import com.smeup.utilities.POIUtilities;
+
+import Smeup.smeui.uiutilities.UIXmlUtilities;
 
 
 public class UseCustomCommand {
-
+	
 	public static void main(String[] args) throws IOException {
 		final InputStream in = new FileInputStream("src/main/resources/custom_commands/excel/command_template.xlsx");
 		final OutputStream out = new FileOutputStream("src/main/resources/custom_commands/excel/command_output.xlsx");
+		final Workbook wb = WorkbookFactory.create(new File("src/main/resources/custom_commands/excel/command_template.xlsx"));
+		SimpleGridObject sgo = new SimpleGridObject(UIXmlUtilities.buildDocumentFromXmlFile("src/main/resources/xml/richimg.xml"));
+		
 		
 		Transformer transformer = TransformerFactory.createTransformer(in, out);
 		AreaBuilder areaBuilder = new XlsCommentAreaBuilder(transformer);
@@ -34,11 +42,20 @@ public class UseCustomCommand {
 		XlsCommentAreaBuilder.addCommandMapping(StyleCommand.COMMAND_NAME, StyleCommand.class);
 		XlsCommentAreaBuilder.addCommandMapping(TemplateCommand.COMMAND_NAME, TemplateCommand.class);
 		List<Area> xlsAreaList = areaBuilder.build();
-		Area xlsArea = xlsAreaList.get(0);
 		Context context = new Context();
-		
+		context = POIUtilities.cercaFun(context, wb.getSheet("FUN"));
 		context.putVar("x", 5);
-		xlsArea.applyAt(new CellRef("Sheet1!A1"), context);
+		context.putVar("sgo_test", sgo.getFormattedValueForCellJXLS(0, sgo.getColumnByIndex(0).getCod()));
+		
+		/*
+		 * Se si aggiungono dei comandi custom, occorre applicare manualmente ciascuna XlsArea a cella e foglio
+		 * a cui appartiene.
+		 */
+		for (Area xlsArea : xlsAreaList) {
+			xlsArea.applyAt(xlsArea.getStartCellRef(), context);
+		}
+		
+		// Corrispettivo di JxlsHelper.getInstance().processGridTemplate() ?
 		transformer.write();
 		in.close();
 		out.close();
